c944145c73d4889d3fa8c9689cd1d9a541c60f36
mskalisz <mskalisz3@gmail.com>
2022-04-03 21:27:50 -0400

    Add answers as well as LS solutions

---
+++ url_components/1.rb
@@ -0,0 +1,24 @@
+=begin
+Given the following URL:
+
+https://amazon.com/Double-Stainless-Commercial-Refrigerator/B60HON32?ie=UTF8&qid=142952676&sr=93&keywords=commercial+fridge
+
+a. Identify the host
+amazon.com
+
+b. Identify the names of the query parameters
+ie qid sr keywords
+
+c. Identify the values of the query parameters
+UTF8 142952676 93 commercial+fridge
+
+d. Identify the scheme
+https://
+
+e. Identify the path
+/Double-Stainless-Commercial-Refrigerator/B60HON32
+
+f. Identify the port
+default: 80
+Incorrect. LS solution This URL does not contain a port. Most software will use port 443 by default when working with this URL due to it having a scheme of https, but that information is not contained within the URL.
+=end
