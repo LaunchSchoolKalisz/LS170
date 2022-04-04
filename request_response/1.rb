2e6d94adc69ebecac303b6207dfd2cc0fb80be88
mskalisz <mskalisz3@gmail.com>
2022-04-03 22:12:02 -0400

    Add LS solution for exercise 1

--- request_response/1.rb
+++ request_response/1.rb
@@ -3,4 +3,9 @@ What are the required components of an HTTP request? What are the additional opt
 
 Required: source, host, path
 Additional: parameters, header, port
+
+LS Solution
+The HTTP method and the path are required, and form part of what is known as the start-line or request-line. As of HTTP 1.0, the HTTP version also forms part of the request-line. The Host header is a required component since HTTP 1.1. Parameters, all other headers, and message body are optional.
+
+Technically speaking the 'path' portion of the request-line is known as the 'request-URI', and incorporates the actual path to the resource and the optional parameters if present. In practice, most people simply refer to this part of the request-line as the 'path'.
 =end
