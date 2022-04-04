c463469823f20e334210a0abe277b4a991eda91a
mskalisz <mskalisz3@gmail.com>
2022-04-03 22:15:28 -0400

    Add solution for exercise 3

--- request_response/3.rb
+++ request_response/3.rb
@@ -1,3 +1,5 @@
 =begin
 What determines whether a request should use GET or POST as its HTTP method?
+
+What the client is asking from the server. It will use GET if it wants information back, or POST if it wants to push information to the server
 =end
