c921647aaf970b1c99ad4ad1d5d697c4545f348b
mskalisz <mskalisz3@gmail.com>
2022-04-14 08:28:21 -0400

    Add a problem with 3 conditional branches

--- bash_exercises/if_statement.sh
+++ bash_exercises/if_statement.sh
@@ -11,12 +11,23 @@
 # fi
 
 # TWO CONDITIONAL BRANCHES
+# integer=9
 
-integer=9
+# if [[ $integer -lt 10 ]]
+# then 
+#   echo $integer is less than 10
+# else
+#   echo $integer is not less than 10
+# fi
 
+# THREE CONDITIONAL BRANCHES
+integer=25
 if [[ $integer -lt 10 ]]
-then 
+then
   echo $integer is less than 10
+elif [[ $integer -gt 20 ]]
+then
+  echo $integer is greater than 20
 else
-  echo $integer is not less than 10
+  echo $integer is between 10 and 20
 fi
