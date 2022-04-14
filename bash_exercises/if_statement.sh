36f49df9e98d36e0cd46e49cc9ca181265281481
mskalisz <mskalisz3@gmail.com>
2022-04-14 08:21:59 -0400

    Comment out first example and add headers for each example

--- bash_exercises/if_statement.sh
+++ bash_exercises/if_statement.sh
@@ -1,10 +1,14 @@
-integer=4
+# NESTED IF STATEMENTS
+# integer=4
 
-if [[ $integer -lt 10 ]]
-  then 
-    echo $integer is less than 10.
-    if [[ $integer -lt 5 ]]
-      then
-        echo $integer is also less than 5
-    fi
-fi
+# if [[ $integer -lt 10 ]]
+#   then 
+#     echo $integer is less than 10.
+#     if [[ $integer -lt 5 ]]
+#       then
+#         echo $integer is also less than 5
+#     fi
+# fi
+
+# TWO CONDITIONAL BRANCHES
+
